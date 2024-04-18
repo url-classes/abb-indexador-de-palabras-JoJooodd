d4a2f117dbc0010fa67766fa7f9093732e0cfd73
JoJooodd <josubauti@outlook.com>
2024-04-18 09:07:58 -0600

    Creacion de main
    Se agrego un archvio de ejemplo y el main respectivamente

---
+++ main.py
@@ -0,0 +1,40 @@
+
+# Abre el archivo en modo de lectura ('r')
+with open('ejemplo.txt', 'r') as file:
+    # Lee todo el contenido del archivo
+    contenido = file.read()
+    print(contenido)
+
+
+def comparar_alfabeticamente(texto1, texto2):
+    palabras1 = texto1.split()
+    palabras2 = texto2.split()
+
+    for palabra1, palabra2 in zip(palabras1, palabras2):
+        if palabra1 < palabra2:
+            return f"{texto1} va primero"
+        elif palabra1 > palabra2:
+            return f"{texto2} va primero"
+
+    # Si las palabras son iguales hasta el momento,
+    # se determina por la longitud total del texto
+    if len(texto1) < len(texto2):
+        return f"{texto1} va primero"
+    elif len(texto1) > len(texto2):
+        return f"{texto2} va primero"
+    else:
+        return "Ambos textos son iguales en términos alfabéticos"
+
+
+def main():
+    # Ejemplo de uso
+    texto1 = "hola"
+    texto2 = "perro"
+    print(comparar_alfabeticamente(texto1, texto2))  # Salida: hola va primero
+
+    texto3 = "holap"
+    texto4 = "holato"
+    print(comparar_alfabeticamente(texto3, texto4))  # Salida: holap va primero
+
+
+main()
